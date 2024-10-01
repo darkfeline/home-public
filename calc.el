3d29f4476f07212cdd3cf59aa496070eacf05ba7
Allen Li <darkfeline@felesatra.moe>
2024-10-01 13:43:31 +0200

    Import

---
+++ calc.el
@@ -0,0 +1,5 @@
+
+;;; Mode settings stored by Calc on Sat Sep 23 15:12:06 2017
+(setq calc-window-height 8)
+(setq calc-date-format '(ZYYY "-" MM "-" DD " " Www (" " hh ":" mm)))
+;;; End of mode settings
