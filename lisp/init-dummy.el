d596f91d291e52f40380dd5f3c9f3a989754b3af
Allen Li <darkfeline@felesatra.moe>
2024-10-01 13:45:23 +0200

    Import

---
+++ lisp/init-dummy.el
@@ -0,0 +1,28 @@
+;;; init-dummy.el --- Dummy to disable Borg init byte-compile  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Allen Li
+
+;; Author: Allen Li <darkfeline@felesatra.moe>
+;; Keywords: local
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Dummy to disable Borg init byte-compile.
+
+;;; Code:
+
+(provide 'init-dummy)
+;;; init-dummy.el ends here
