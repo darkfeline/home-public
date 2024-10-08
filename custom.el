516649414a4267c23ba2dcbc6fdc793f2594cee1
Allen Li <darkfeline@felesatra.moe>
2024-10-08 15:58:53 -0700

    Update packages

--- custom.el
+++ custom.el
@@ -268,7 +268,7 @@
  '(package-install-upgrade-built-in t)
  '(package-quickstart t)
  '(package-selected-packages
-   '(bazel gptel bash-completion systemd dockerfile-mode smartparens dash bluetooth diff-hl tiny keycast aggressive-indent project expand-region htmlize rainbow-mode ws-butler wgrep csv-mode async with-editor yasnippet seq gnu-elpa-keyring-update pinentry marginalia valign vlf vundo orderless consult vertico org flymake eglot))
+   '(ddskk dumb-jump helpful xterm-color bazel gptel bash-completion systemd dockerfile-mode smartparens bluetooth diff-hl tiny keycast aggressive-indent project expand-region htmlize rainbow-mode ws-butler wgrep csv-mode async with-editor yasnippet seq gnu-elpa-keyring-update pinentry marginalia valign vlf vundo orderless consult vertico org flymake eglot))
  '(package-unsigned-archives '("melpa" "melpa-stable"))
  '(project-list-file "~/.local/state/emacs/projects")
  '(project-switch-commands
