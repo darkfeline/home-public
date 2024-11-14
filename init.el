2bc4ede607bc93f08c17cdd44d22dcf181cf954d
Allen Li <darkfeline@felesatra.moe>
2024-11-14 14:03:25 -0800

    Migrate go-mode to vc

--- init.el
+++ init.el
@@ -6,6 +6,7 @@
 (require 'mir-init)
 
 (mir-init-bootstrap-package-vc '(easydraw
+                                 go-mode
                                  magit
                                  reintegrate
                                  jakuri))
